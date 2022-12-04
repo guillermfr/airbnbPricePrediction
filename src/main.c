ec923eed5542589effcf44c378c5c260215f2b30
François <81768287+guillermfr@users.noreply.github.com>
2022-12-04 18:43:51 +0100

    Final version of main (for now)

--- src/main.c
+++ src/main.c
@@ -2,10 +2,28 @@
 
 int main(int argc, char** argv) {
 
+    srand(time(NULL));
+    CLEAR_CONSOLE
+
     int tailleTab;
     logement* tableau;
 
     tableau = createArray(&tailleTab);
+
+    logement logementAComparer = inputAccomodation();
+
+    distance(tableau, tailleTab, logementAComparer);
+
+    randomizeArray(tableau, tailleTab);
+    triFusion(tableau, tailleTab);
+
+    printf("Le tableau a été trié par ordre de distance croissante.\n");
+
+    float price = priceCalculation(tableau, tailleTab);
+
+    printf("Le prix ainsi calculé est de %.2f€.\n", price);
+
+    free(tableau);
     
     return 0;
 }
