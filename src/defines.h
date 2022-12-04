3caa7399317cb95ab55cf2b2fc9436848ca2382c
François <81768287+guillermfr@users.noreply.github.com>
2022-12-04 18:26:45 +0100

    Added 2 new macros

--- src/defines.h
+++ src/defines.h
@@ -3,5 +3,6 @@
 
 #define FILE_NAME "data/airbnb_donnees_propre.csv"
 #define CLEAR_CONSOLE printf("\e[1;1H\e[2J");
+#define AFFICHAGE_ATTRIBUTS_COMPARER printf("1. Logements\n2. Chambres\n3. Salles de bain\n4. Lits\n5. Nombre minimum de nuits\n6. Nombre maximum de nuits\n");
 
 #endif
